4dcf671120c2c8f9839cd9d3b4a7b38b5be01459
trilok <trilokkambham2001@gmail.com>
2025-02-23 10:11:47 -0600

    final commit

--- PriceQuotation/obj/Debug/net8.0/PriceQuotation.AssemblyInfo.cs
+++ PriceQuotation/obj/Debug/net8.0/PriceQuotation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PriceQuotation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f73698120862717e6cb9c304e6030236159911c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42de1a32ab92d8f25708ab4852b6bcd2383d5fb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PriceQuotation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PriceQuotation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
